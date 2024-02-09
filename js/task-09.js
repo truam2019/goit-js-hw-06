145ba78662686d8dd5baeaa961f9c317acd98cb0
truam2019 <53709957+truam2019@users.noreply.github.com>
2024-02-08 20:29:38 -0500

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,12 +1,13 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+const body = document.querySelector('body');
+const color = document.querySelector('span.color')
+const colorChangerButton = document.querySelector('button.change-color');
 
-function changeBackgroundColor() {
-  const body = document.body;
-  const colorSpan = document.querySelector('.color');
-
-  const newColor = getRandomHexColor();
-  body.style.backgroundColor = newColor;
-  colorSpan.textContent = newColor;
-}
+colorChangerButton.addEventListener('click', () => {
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+})
